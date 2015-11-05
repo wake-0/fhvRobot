2b5e5c7fa56c81a1765ee2047305dd8590e585fa
Nicolaj Hoess <nicohoess@gmail.com>
2015-11-05 12:05:42 +0100

    Ticket #11 - Implemented debugger class

---
+++ Source_Robot/include/Debugger.h
@@ -0,0 +1,38 @@
+#ifndef UTIL_DEBUGGER_H_
+#define UTIL_DEBUGGER_H_
+
+#include <string>
+#include <iostream>
+
+#define _DEBUG
+#define CAKE_DebugLevel VERBOSE
+
+#define ERROR 0
+#define WARNING 2
+#define INFO 5
+#define VERBOSE 10
+
+class Debugger {
+public:
+    Debugger(int level)
+#ifdef _DEBUG
+    : m_output( level <= CAKE_DebugLevel )
+#endif
+    { }
+    template<typename T>
+    Debugger& operator<<(T t) {
+        #ifdef _DEBUG
+        if (m_output) {
+            std::cout << t;
+            return *this;
+        } else
+        #endif
+        return *this;
+    }
+private:
+#ifdef _DEBUG
+    bool m_output;
+#endif
+};
+
+#endif /* UTIL_DEBUGGER_H_ */
