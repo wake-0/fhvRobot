49c6621e59fe4808820868861c26c637a2a45e5c
Kevin <kevin.wallis@hotmail.com>
2015-12-07 17:27:53 +0100

    Bug #23 - Implemented suggested easy fix.

--- Source_Server/RoboServer.Network/src/communication/managers/SessionManager.java
+++ Source_Server/RoboServer.Network/src/communication/managers/SessionManager.java
@@ -28,6 +28,8 @@ public class SessionManager extends LayerManager<SessionPDU> {
 	private final byte initConnectionSession = (byte) 0b00000000;
 	private final byte defaultConnectionFlags = (byte) 0b00000000;
 
+	private final int maxNumberOfIterations = 200;
+
 	// Constructor
 	public SessionManager(IConfigurationManager manager, CurrentConfigurationService currentClientService) {
 		super(manager, currentClientService);
@@ -56,21 +58,28 @@ public class SessionManager extends LayerManager<SessionPDU> {
 			// TODO: create a new create SessionNumberAlgorithm
 			// because the old one is not checking the already used session ids
 			int newIntSession = createNewSessionNumber(currentConfiguration.getSessionId(), alreadyUsedSessions);
-			byte newByteSession = NumberParser.intToByte(newIntSession);
+			if (newIntSession == -1) {
+				// Send rejected as answer
+				DatagramPacket answerPacket = createNoFreeSlotPacket();
+				sender.answer(currentConfiguration, answerPacket);
+			} else {
+				byte newByteSession = NumberParser.intToByte(newIntSession);
 
-			// Set the session id for the configuration
-			currentConfiguration.setSessionId(newIntSession);
+				// Set the session id for the configuration
+				currentConfiguration.setSessionId(newIntSession);
 
-			// Create answer pdu
-			byte answerFlags = initConnectionFlags;
-			byte answerSessionId = newByteSession;
-			byte[] answer = new byte[] { answerFlags, answerSessionId };
+				// Create answer pdu
+				byte answerFlags = initConnectionFlags;
+				byte answerSessionId = newByteSession;
+				byte[] answer = new byte[] { answerFlags, answerSessionId };
 
-			// Send answer pdu
-			sender.answer(currentConfiguration, answer);
+				// Send answer pdu
+				sender.answer(currentConfiguration, answer);
+			}
 
 			// Everything is handled so no need to go to the upper layers
 			handled = true;
+
 		} else if (flags == initConnectionFlags && sessionId != initConnectionSession) {
 			// Set session id
 			currentConfiguration.setSessionId(pdu.getSessionId());
@@ -81,14 +90,25 @@ public class SessionManager extends LayerManager<SessionPDU> {
 	}
 
 	private int createNewSessionNumber(int oldSessionNumber, List<Integer> notAllowedSessions) {
+		int numberOfIterations = 0;
 		int newNumber = oldSessionNumber;
 		{
 			// Create new session number between min and max
 			newNumber = ThreadLocalRandom.current().nextInt(minSessionNumber, maxSessionNumber + 1);
+			numberOfIterations++;
 		}
-		while (newNumber == oldSessionNumber || notAllowedSessions.contains(newNumber))
+		while ((newNumber == oldSessionNumber || notAllowedSessions.contains(newNumber))
+				&& numberOfIterations < maxNumberOfIterations)
 			;
 
-		return newNumber;
+		return numberOfIterations >= maxNumberOfIterations ? -1 : newNumber;
+	}
+
+	private DatagramPacket createNoFreeSlotPacket() {
+		byte answerFlags = 0;
+		byte answerSessionId = 0;
+		byte[] answer = new byte[] { answerFlags, answerSessionId };
+		DatagramPacket answerPacket = new DatagramPacket(answer, answer.length);
+		return answerPacket;
 	}
 }
