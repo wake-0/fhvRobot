56608902be9e78c2c1c280173f0606a8319074ea
Nicolaj Hoess <nicohoess@gmail.com>
2015-12-13 23:22:33 +0100

    Ticket #17 - Reversed motor commands

--- Source_Server/RoboServer.ApplicationProtocol/src/communication/commands/Commands.java
+++ Source_Server/RoboServer.ApplicationProtocol/src/communication/commands/Commands.java
@@ -4,8 +4,8 @@ public class Commands {
 	public static final int CHANGE_NAME = 1;
 	public static final int GENERAL_MESSAGE = 2;
 
-	public static final int DRIVE_LEFT = 10;
-	public static final int DRIVE_RIGHT = 11;
+	public static final int DRIVE_LEFT = 11;
+	public static final int DRIVE_RIGHT = 10;
 	public static final int DRIVE_BOTH = 12;
 
 	public static final int ROBO_STEARING = 50;
