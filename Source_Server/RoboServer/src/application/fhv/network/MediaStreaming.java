c8e2534485a632ac3f38103487b6f50e4f827e63
Kevin <kevin.wallis@hotmail.com>
2015-12-26 16:32:59 +0100

    Bug #59 - Added lib and first test for the streaming to the server.

--- Source_Server/RoboServer/src/application/fhv/network/MediaStreaming.java
+++ Source_Server/RoboServer/src/application/fhv/network/MediaStreaming.java
@@ -4,10 +4,16 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 
+import com.sun.jna.NativeLibrary;
+
 import network.receiver.INetworkReceiver;
 import network.receiver.LoggerNetworkReceiver;
 import network.sender.INetworkSender;
 import network.sender.LoggerNetworkSender;
+import uk.co.caprica.vlcj.binding.LibVlc;
+import uk.co.caprica.vlcj.player.MediaPlayerFactory;
+import uk.co.caprica.vlcj.player.headless.HeadlessMediaPlayer;
+import uk.co.caprica.vlcj.runtime.RuntimeUtil;
 
 public class MediaStreaming implements Runnable {
 
@@ -19,6 +25,7 @@ public class MediaStreaming implements Runnable {
 	protected final DatagramSocket socket;
 
 	private final int packetSize = 64000;
+	private static final String NATIVE_LIBRARY_SEARCH_PATH = "C:\\Program Files\\VideoLAN\\VLC";
 
 	public MediaStreaming(int mediaStreamingPort) throws SocketException {
 		this.socket = new DatagramSocket(mediaStreamingPort);
@@ -30,6 +37,9 @@ public class MediaStreaming implements Runnable {
 	public void run() {
 		isRunning = true;
 
+		NativeLibrary.addSearchPath(RuntimeUtil.getLibVlcLibraryName(), NATIVE_LIBRARY_SEARCH_PATH);
+		System.out.println(LibVlc.INSTANCE.libvlc_get_version());
+
 		while (isRunning) {
 			byte[] receiveData = new byte[packetSize];
 			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
@@ -40,8 +50,27 @@ public class MediaStreaming implements Runnable {
 			}
 
 			System.out.println("length=" + receivePacket.getLength());
-			// sender.send(receivePacket);
+
+			String media = "udp://@:13337";
+			String options = formatHttpStream("127.0.0.1", 5555);
+
+			System.out.println("Streaming '" + media + "' to '" + options + "'");
+
+			MediaPlayerFactory mediaPlayerFactory = new MediaPlayerFactory("");
+			HeadlessMediaPlayer mediaPlayer = mediaPlayerFactory.newHeadlessMediaPlayer();
+			mediaPlayer.playMedia(media, options);
 		}
+	}
+
+	private static String formatHttpStream(String serverAddress, int serverPort) {
+		StringBuilder sb = new StringBuilder(60);
+		sb.append(":sout=#duplicate{dst=std{access=http,mux=ts,");
+		sb.append("dst=");
+		sb.append(serverAddress);
+		sb.append(':');
+		sb.append(serverPort);
+		sb.append("}}");
+		return sb.toString();
 	}
 
 	public void stop() {
