7df026463458d18604e427fe6da1d8009a9a0b6a
Kevin <kevin.wallis@hotmail.com>
2015-12-26 17:37:00 +0100

    Bug #59 - Added lib and first test for the streaming to the server.

--- Source_Server/RoboServer/src/application/fhv/network/MediaStreaming.java
+++ Source_Server/RoboServer/src/application/fhv/network/MediaStreaming.java
@@ -1,19 +1,14 @@
 package network;
 
-import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
-
-import com.sun.jna.NativeLibrary;
 
 import network.receiver.INetworkReceiver;
 import network.receiver.LoggerNetworkReceiver;
 import network.sender.INetworkSender;
 import network.sender.LoggerNetworkSender;
-import uk.co.caprica.vlcj.binding.LibVlc;
 import uk.co.caprica.vlcj.player.MediaPlayerFactory;
 import uk.co.caprica.vlcj.player.headless.HeadlessMediaPlayer;
-import uk.co.caprica.vlcj.runtime.RuntimeUtil;
 
 public class MediaStreaming implements Runnable {
 
@@ -23,9 +18,6 @@ public class MediaStreaming implements Runnable {
 	protected final INetworkReceiver receiver;
 	protected final INetworkSender sender;
 	protected final DatagramSocket socket;
-
-	private final int packetSize = 64000;
-	private static final String NATIVE_LIBRARY_SEARCH_PATH = "C:\\Program Files\\VideoLAN\\VLC";
 
 	public MediaStreaming(int mediaStreamingPort) throws SocketException {
 		this.socket = new DatagramSocket(mediaStreamingPort);
@@ -37,22 +29,9 @@ public class MediaStreaming implements Runnable {
 	public void run() {
 		isRunning = true;
 
-		NativeLibrary.addSearchPath(RuntimeUtil.getLibVlcLibraryName(), NATIVE_LIBRARY_SEARCH_PATH);
-		System.out.println(LibVlc.INSTANCE.libvlc_get_version());
-
 		while (isRunning) {
-			byte[] receiveData = new byte[packetSize];
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			receiver.receive(receivePacket);
-
-			if (socket.isClosed()) {
-				continue;
-			}
-
-			System.out.println("length=" + receivePacket.getLength());
-
 			String media = "udp://@:13337";
-			String options = formatHttpStream("127.0.0.1", 5555);
+			String options = formatHttpStream("127.0.0.1", 990);
 
 			System.out.println("Streaming '" + media + "' to '" + options + "'");
 
