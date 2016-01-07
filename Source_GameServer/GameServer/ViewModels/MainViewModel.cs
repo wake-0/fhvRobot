e891c6f2a36bdb36bfca961d427ed1a97eca3d6e
Kevin <kevin.wallis@hotmail.com>
2016-01-07 18:56:49 +0100

    Bug #84 - Initialized C# project.

---
+++ Source_GameServer/GameServer/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GameServer.Utils;
+using PostSharp.Patterns.Model;
+
+namespace GameServer.ViewModels
+{
+    [NotifyPropertyChanged]
+    public class MainViewModel
+    {
+        #region properties
+        public string TestText { get; set; }
+        public ICommand ChangeTextCommand { get; set; }
+
+        private const string Text1 = "GameServer";
+        private const string Text2 = "ChangedText";
+        #endregion
+
+        #region ctor
+        public MainViewModel()
+        {
+            TestText = Text1;
+            ChangeTextCommand = new DelegateCommand(ChangeText);
+        }
+
+        private void ChangeText(object obj)
+        {
+            TestText = TestText == Text1 ? Text2 : Text1;
+        }
+
+        #endregion
+
+    }
+}
