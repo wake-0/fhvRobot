2665077db54c5013dc482d9754520eab34078621
Kevin <kevin.wallis@hotmail.com>
2016-01-12 21:51:09 +0100

    Ticket #92 - Implemented score manager.

--- Source_GameServer/GameServer/ViewModels/ScoreManager.cs
+++ Source_GameServer/GameServer/ViewModels/ScoreManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using GameServer.Models;
 using PostSharp.Patterns.Model;
 
@@ -10,6 +12,8 @@ namespace GameServer.ViewModels
         #region Properties
         public ObservableCollection<Score> TopScores { get; private set; }
         public ObservableCollection<Score> Scores { get; private set; }
+
+        public static readonly int NUMBER_TOP_SCORES = 3;
         #endregion
 
         #region ctor
@@ -23,12 +27,45 @@ namespace GameServer.ViewModels
         #region Methods
         public void Add(Score score)
         {
-            if (TopScores.Count < 3)
+            if (score == null) throw new ArgumentNullException("score");
+
+            if (TopScores.Count < NUMBER_TOP_SCORES)
             {
-                TopScores.Add(score);
+                // Insert new top score
+                InsertScoreAtPosition(TopScores, score);
+            } 
+            else if (TopScores.Any(s => s.Duration > score.Duration))
+            {
+                // Take last entry
+                var worstScore = TopScores.Last();
+                TopScores.Remove(worstScore);
+
+                // Insert worst score to normal list at the beginning
+                Scores.Insert(0, worstScore);
+
+                // Insert new top score
+                InsertScoreAtPosition(TopScores, score);
+            }
+            else
+            {
+                // Insert into normal list
+                InsertScoreAtPosition(Scores, score);
+            }
+        }
+
+        private void InsertScoreAtPosition(ObservableCollection<Score> scores, Score score)
+        {
+            if (scores == null) throw new ArgumentNullException("scores");
+
+            for (var i = 0; i < scores.Count; i++)
+            {
+                if (scores.ElementAt(i).Duration <= score.Duration) continue;
+
+                scores.Insert(i, score);
+                return;
             }
 
-            Scores.Add(score);
+            scores.Add(score);
         }
         #endregion
     }
