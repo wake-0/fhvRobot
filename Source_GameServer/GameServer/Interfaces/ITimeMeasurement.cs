45c7dcc4c7dacad55725c6750c5d3d2e51a0bc2c
Kevin <kevin.wallis@hotmail.com>
2016-01-13 19:52:26 +0100

    Ticket #96 - Added ITimeMeasurementSystem.

---
+++ Source_GameServer/GameServer/Interfaces/ITimeMeasurement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GameServer.Interfaces
+{
+    public interface ITimeMeasurement
+    {
+        bool IsSystemActive { get; }
+        event EventHandler<TimeSpan> TimeMeasured;
+ 
+        bool ActivateSystem();
+        bool DeactivateSystem();
+    }
+}
