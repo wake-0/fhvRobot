8e065e55591b201e3bcc653a8ae874a9a469dfa4
Kevin <kevin.wallis@hotmail.com>
2016-01-13 19:29:39 +0100

    Ticket #95 - Added player position.

---
+++ Source_GameServer/GameServer/Converters/IndexConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace GameServer.Converters
+{
+    public class IndexConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var item = value as ListViewItem;
+            var listView = ItemsControl.ItemsControlFromItemContainer(item) as ListView;
+            if (item == null || listView == null) return null;
+
+            var offset = 0;
+            if (parameter != null)
+            {
+                int.TryParse(parameter as string, out offset);
+            }
+
+            var index = listView.ItemContainerGenerator.IndexFromContainer(item) + offset;
+            return index.ToString() + ".";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
