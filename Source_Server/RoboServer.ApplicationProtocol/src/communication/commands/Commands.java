9298986c451f18e75966332229fda859ff3e9930
pete <petrovic-marko@hotmail.com>
2016-01-21 12:59:03 +0100

    BUG #76 defined command id 18 for triggering led

--- Source_Server/RoboServer.ApplicationProtocol/src/communication/commands/Commands.java
+++ Source_Server/RoboServer.ApplicationProtocol/src/communication/commands/Commands.java
@@ -10,6 +10,7 @@ public class Commands {
 	public static final int DRIVE_LEFT = 11;
 	public static final int DRIVE_RIGHT = 10;
 	public static final int DRIVE_BOTH = 12;
+	public static final int TRIGGER_LED = 18;
 
 	public static final int ROBO_STEARING = 50;
 	public static final int ROBO_NOT_STEARING = 51;
