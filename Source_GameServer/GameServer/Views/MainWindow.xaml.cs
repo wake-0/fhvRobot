9ca4d0b24d841af1222e57f441bcda7545610c19
pete <petrovic-marko@hotmail.com>
2016-01-24 12:34:14 +0100

    BUG #131 implemented auto-scroll for every newly added score

--- Source_GameServer/GameServer/Views/MainWindow.xaml.cs
+++ Source_GameServer/GameServer/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using GameServer.ViewModels;
+using System.Windows;
+using GameServer.Models;
 
 namespace GameServer.Views
 {
@@ -10,6 +12,16 @@ namespace GameServer.Views
         public MainWindow()
         {
             InitializeComponent();
+            ((MainViewModel)DataContext).ScoreManager.Scores.CollectionChanged += Scores_CollectionChanged;
+        }
+
+        void Scores_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (ScoreListView.Items.Count > 1)
+            {
+                var item = ScoreListView.Items[ScoreListView.Items.Count - 1];
+                ScoreListView.ScrollIntoView((Score)item);
+            }
         }
     }
 }
