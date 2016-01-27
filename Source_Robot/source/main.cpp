94c0d1c0ae5013ce3bcf8f6f0a38d232960ffb27
Nicolaj Hoess <nicohoess@gmail.com>
2016-01-27 12:25:51 +0100

    Tickt #111 - Removed hello-world comment from main

--- Source_Robot/source/main.cpp
+++ Source_Robot/source/main.cpp
@@ -1,21 +1,3 @@
-/*
- * hellobone.c
- *
- * Version: 1.0
- * Date:	18.10.2013
- *
- * Copyright (c) 2013, jkuhlm - All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * In Eclipse add Include path
- *     C:\gcc-linaro\arm-linux-gnueabihf\libc\usr\include
- *
- */
-
 #include "../include/Controller.h"
 #include "../include/Debugger.h"
 #include "../include/sensors/FusionFilter.h"
