d34183bf4a401adc5063e15336786f6035c0ca0f
Nicolaj Hoess <nicohoess@gmail.com>
2016-01-29 10:17:11 +0100

    Release 1.0

--- Source_App/app/src/main/java/app/robo/fhv/roboapp/communication/GlobalSettings.java
+++ Source_App/app/src/main/java/app/robo/fhv/roboapp/communication/GlobalSettings.java
@@ -10,5 +10,6 @@ public class GlobalSettings
     public static int SERVER_PORT = 997;
     //public static String SERVER_ADDRESS = "83.212.127.13";
     public static String SERVER_ADDRESS = "172.22.27.216";
+    //public static String SERVER_ADDRESS = "10.0.0.44";
     public static String LOCAL_ADDRESS = "10.0.2.2";
 }
